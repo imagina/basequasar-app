2ee1ae2ca1d9ec325a1405851e53a5a0004ca03b
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-05-21 11:25:55 -0500

    +Update: env.js, if not exist BASE_URL on .env, add dynamic about url "https://www.backend.domainname.com"

--- env/env.js
+++ env/env.js
@@ -1,7 +1,12 @@
-export default function(key, fallback){
+export default function (key, fallback) {
   let value = process.env[key]
-  if (!value) {
-    return fallback
+  //If not found base url, do dynamic url
+  if ((key == 'BASE_URL') && !value){
+    let domain = window.location.host.split('.')
+    if(domain[0] == 'www') domain[0] = 'backend'
+    else domain.unshift('backend')
+    value = 'https://www.'+domain.join('.')
   }
+  if (!value) return fallback//Check if must return fallback
   return value.replace(/["]/g, '')
 }
