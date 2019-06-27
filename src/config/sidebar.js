c4142a603d75a2746aa3f7224af91266fcadadf9
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-06-27 16:55:29 -0500

    +Update: autoload sidebars of modules in project when is backend

--- src/config/sidebar.js
+++ src/config/sidebar.js
@@ -1,6 +1,25 @@
-import indexSidebar from '@imagina/qmenu/_config/sidebar/index'//Side bar of qMenu
 const pages = config('pages') // Get Pages from config
+const appConfig = config('app')
 
+//Autoload sidebars of modules
+//Not edit
+let localSidebar = []//Response
+
+if (appConfig.isBackend) {
+  let modules = appConfig.modules
+  modules.forEach((name) => {
+    try {
+      let moduleSidebar = require(`@imagina/${name}/_config/sidebar`).default
+      if (moduleSidebar && moduleSidebar[0]) {
+        localSidebar = localSidebar.concat(moduleSidebar)
+      }
+    } catch (e) {
+    }
+  })
+}
+
+
+//Add extra items to sidebar
 /*Example group item. April 04, 2019
 {
 	title: "title",
@@ -18,11 +37,10 @@ const pages = config('pages') // Get Pages from config
 }
 */
 
-
 //Add items tu sidebar
 let sidebar = [
   pages.app.home,//Home
 ]
 
 //Return merge between local sidebar and sidebar of qMenu
-export default sidebar.concat(indexSidebar)
+export default sidebar.concat(localSidebar)
