435a727b91ec023dea38361b74f4ac640e5c7add
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-11-26 10:55:00 -0500

    +Update: remove condition to key "BASE_URL" in env.js +Add: qbanner to package json

--- env/env.js
+++ env/env.js
@@ -1,12 +1,5 @@
 export default function (key, fallback) {
   let value = process.env[key]
-  //If not found base url, do dynamic url
-  if ((key == 'BASE_URL') && !value){
-    let domain = window.location.host.split('.')
-    if(domain[0] == 'www') domain[0] = 'backend'
-    else domain.unshift('backend')
-    value = 'https://'+domain.join('.')
-  }
   if (!value) return fallback//Check if must return fallback
   return value.replace(/["]/g, '')
 }
