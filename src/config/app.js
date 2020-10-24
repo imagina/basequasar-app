941d59e0110918020dba7d95914ce761df77d174
msolanogithub <msolano2505@gmail.com>
2020-10-24 15:10:36 -0500

    +Update: Version 2.2.7 +Update: Setting Admin page | Load dynamic settings fields

--- src/config/app.js
+++ src/config/app.js
@@ -2,7 +2,7 @@ import coreApp from '@imagina/qsite/_config/master/app'
 
 export default {
   ...coreApp,
-  version : '2.2.5',
+  version : '2.2.7',
   //isBackend : false, //Define if project is to admin
   //forceRoleAndDepartment : false,//Force to select role and department
   //UI Languages
