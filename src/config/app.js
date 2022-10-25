c987677e6c1d6c84be3fe5f41cf35af4630af4ec
msolano <msolano@imaginacolombia.com>
2022-10-25 16:22:33 -0500

    +Release

--- src/config/app.js
+++ src/config/app.js
@@ -4,7 +4,8 @@ export default {
   ...coreApp,
   //version: '2.3.29', //Version
   //git submodule foreach --recursive git merge dev
-  // baseUrl: 'https://localhost:7116',//Define base url
+  //baseUrl: 'https://dev-booking-test.ozonohosting.com',//Define base url
+  //baseUrl: 'https://dev-colchones-amanecer.ozonohosting.com',//Define base url
   //mode: 'ipanel', //Define load mode
   //forceRoleAndDepartment : false,//Force to select role and department
   //UI Languages
