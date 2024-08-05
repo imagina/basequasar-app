8f6977ab859497239a092d7c0764210532261876
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-08-05 11:10:25 -0500

    Refactor of the service worker (SW):
    + Filtering the resources saved in the pre-cache.
    + Ensuring the caching of fonts.
    + Modularizing the connection to IndexedDB.
    + Validating the setting (isite::offline) for enabling/disabling the SW.
    + Changing event names.
    + Saving pending requests in IndexedDB under the key `requests`.

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -12,13 +12,14 @@ declare const self: ServiceWorkerGlobalScope &
 
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 // import { registerRoute } from 'workbox-routing';
-import { ExpirationPlugin } from 'workbox-expiration';
+//import { ExpirationPlugin } from 'workbox-expiration';
 // import { NetworkFirst, CacheFirst } from 'workbox-strategies';
 import { Queue } from 'workbox-background-sync';
 import { clientsClaim, setCacheNameDetails } from 'workbox-core';
+import { googleFontsCache } from 'workbox-recipes';
+import moduleList from 'src/setup/modules.json';
 
-cleanupOutdatedCaches()
-
+self.skipWaiting()
 // clientsClaim() is used so that a new Service Worker
 // takes control of existing client windows (browser tabs)
 // that are still controlled by a previous Service Worker.
@@ -31,11 +32,42 @@ setCacheNameDetails({
 })
 
 // Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST);
+const wbManifest = [ ...self.__WB_MANIFEST ]
+
+const modules = moduleList.modules;
+
+const filterRoutes = (routes: any, modules: string[]) => {
+  // filtering resources to cache
+  return routes.filter(asset => {
+    if (asset.url.includes('src_modules')) {
+      return modules.some(module => asset.url.includes(module));
+    }
+    return true;
+  });
+}
+
+// Use with precache injection
+precacheAndRoute(filterRoutes(wbManifest, modules));
+
+cleanupOutdatedCaches()
+
+googleFontsCache();
 
 const requestPOST = new Map<string, Request>();
 const sentPOST = new Map<string, any>();
 const QUEUE_NAME = 'requests';
+const status = {
+  pending: 'PENDING',
+  successful: 'SUCCESSFUL',
+  failed: 'FAILED',
+}
+const methods = {
+  post: 'POST',
+  put: 'PUT',
+  delete: 'DELETE',
+}
+const NAME_OBJECT_STORE = 'storage';
+const KEY_REQUESTS_IN_STORAGE = 'requests';
 // const CACHE_NAME = 'runtime-cache';
 
 // const plugins = [
@@ -163,8 +195,9 @@ const prepareRequest = async (entry) => {
   try {
     const method = entry.request.method;
     const requestId = entry.metadata?.requestId;
+    const request = entry.request.clone()
 
-    const dataBody = await transformReadableStreamToObject(entry.request.clone().body);
+    const dataBody = JSON.parse(await request.text())
     const idAttribute = dataBody?.attributes?.id;
     const databaseGeneratedId = sentPOST.get(requestId);
 
@@ -176,7 +209,7 @@ const prepareRequest = async (entry) => {
 
     const body = JSON.stringify(dataBody);
 
-    const requestAttributes = method === 'DELETE' ? { body: null, method: 'DELETE' } : { body };
+    const requestAttributes = method === methods.delete ? { body: null, method: methods.delete } : { body };
     const newRequest = await replaceRequestUrlWithStoredUrl(
       entry.request.clone(),
       requestId,
@@ -191,7 +224,7 @@ const prepareRequest = async (entry) => {
 
 const nameDB = () => {
   const hostname = self.location.hostname.split('.')
-  const response = hostname
+  let response: string[] = hostname
 
   //Set capitalize to all words
   hostname.forEach((word, index) => {
@@ -202,33 +235,115 @@ const nameDB = () => {
 
   //Remove .com .org....
   if (hostname.length >= 2) response.pop()
+  response.includes('localhost') && response.push(':8080')
 
   return `${response.join('')}DB`
 }
 
-const updateCacheData = (response) => {
-  try {
-    const openDB = indexedDB.open(nameDB());
-    const KEY = 'apiRoutes.qramp.workOrders::offline'
+const openConnectionWithIndexedDB = () => {
+  return indexedDB.open(nameDB());
+}
 
-    openDB.onsuccess = (event) => {
-      const db = openDB.result;
-      const transaction = db.transaction('storage', 'readwrite')
-      const storage = transaction.objectStore('storage')
-      const request = storage.get(KEY)
+const executeTransaction = (objectStore: string, key: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    try {
+      const openDB = openConnectionWithIndexedDB()
 
-      request.onsuccess = (event) => {
-        const data = request.result?.data;
-        const newData = data.map(item => {
-          const match = String(item.offlineId) === String(response.offlineId)
-          return match ? response : item;
-        })
-        storage.put(
-          { ...request.result, data: newData, },
-          KEY
-        )
+      openDB.onsuccess = (event) => {
+        const db = openDB.result;
+        const transaction = db.transaction(objectStore, 'readwrite')
+        const storage = transaction.objectStore(objectStore)
+        const request = storage.get(key)
+
+        request.onsuccess = (event) => {
+          resolve({ request, storage });
+        }
+
+        request.onerror = (event) => {
+          reject((event.target as any).error);
+        }
       }
+
+      openDB.onerror = (event) => {
+        reject((event.target as any).error);
+      }
+    } catch (error) {
+      reject(error);
     }
+  });
+}
+
+const getOfflineSetting = async (): Promise<boolean | null> => {
+  try {
+    const SITE_SETTINGS = 'qsite.settings'
+    const SETTING = 'isite::offline'
+    const { request } = await executeTransaction(NAME_OBJECT_STORE, SITE_SETTINGS);
+    const storedData = request.result?.data;
+    const offlineSetting = storedData.siteSettings.find(item => item.name === SETTING);
+    return Boolean(offlineSetting.value);
+  } catch (error) {
+    console.error('Error getting offline setting:', error);
+    return null;
+  }
+}
+
+const saveExecutedRequests = async (request, id: number, status: string) => {
+  const requestInfo = {
+    id,
+    requestData: {
+      body: await request.text(),
+      url: request.url,
+      method: request.method,
+      headers: Object.fromEntries(request.headers.entries()),
+    },
+    timestamp: new Date().valueOf(),
+    metadata: {
+      status,
+    }
+  }
+
+  const { request: requestsExecuted, storage } = await executeTransaction(
+    NAME_OBJECT_STORE,
+    KEY_REQUESTS_IN_STORAGE, 
+  )
+
+  const storedData = requestsExecuted.result?.requests || [];
+  storedData.push(requestInfo)
+  storage.put(
+    { requests: storedData, },
+    KEY_REQUESTS_IN_STORAGE
+  )
+}
+
+const updateRequestStatus = async (entry, requestStatus) => {
+  const { request: requestsExecuted, storage } = await executeTransaction(NAME_OBJECT_STORE, KEY_REQUESTS_IN_STORAGE);
+  const storedData = requestsExecuted.result?.requests || [];
+  storedData.map(async (item) => {
+    if (String(item.id) === String(entry.metadata.id)) {
+      item.metadata.status = requestStatus
+    }
+  })
+
+  storage.put(
+    { requests: storedData, },
+    KEY_REQUESTS_IN_STORAGE
+  )
+}
+
+const updateCacheData = async (response, key: string) => {
+  if (!key) return;
+  try {
+    const { request, storage } = await executeTransaction(NAME_OBJECT_STORE, key);
+
+    const data = request.result?.data;
+    const newData = data.map(item => {
+      const match = String(item.id) === String(response.offlineId);
+      return match ? response : item;
+    })
+    storage.put(
+      { ...request.result, data: newData, },
+      key
+    )
   } catch (error) {
     console.error('error', error)
   }
@@ -238,29 +353,40 @@ const queue = new Queue(QUEUE_NAME, {
   onSync: async ({ queue }) => {
     let entry;
     const retryCounters = new Map<string, number>();
+    postMessage('synchronizing-data');
 
+    
     while (entry = await queue.shiftRequest()) {
+      if (!navigator.onLine) {
+        await queue.unshiftRequest(entry); 
+        break
+      }
       try {
         const method = entry.request.method;
+        const body = await entry.request.clone().text();
+        const payload = await JSON.parse(body);
+        const key = `${payload.attributes.api_route}::offline`
 
         if (
-          method === 'PUT' ||
-          method === 'DELETE'
+          method === methods.put ||
+          method === methods.delete
         ) {
           const newRequest = await prepareRequest(entry);
           if (newRequest) entry.request = newRequest;
         }
 
-        const response = await fetch(entry.request);
+        const response = await fetch(entry.request.clone());
 
-        if (method === 'POST') {
+        if (method === methods.post) {
           const { data } = await response.json();
-          if (data?.offlineId) updateCacheData(data)
+          if (data?.offlineId) await updateCacheData(data, key)
           // Save the ID generated by the server
           // to later use it in the URL of the DELETE or PUT
           // request corresponding to the created record.
           sentPOST.set(data?.offlineId, data?.id);
         }
+
+        await updateRequestStatus(entry, status.successful)
 
         postMessage('successful');
 
@@ -273,8 +399,9 @@ const queue = new Queue(QUEUE_NAME, {
 
         if (retryCounter < ATTEMPTS) {
           retryCounters.set(entry.request.url, retryCounter + 1);
-          await queue.unshiftRequest(entry);
+            await queue.unshiftRequest(entry);
         } else {
+          await updateRequestStatus(entry, status.failed)
           retryCounters.delete(entry.request.url);
         }
 
@@ -282,15 +409,15 @@ const queue = new Queue(QUEUE_NAME, {
       }
     }
 
-    postMessage('sync-data');
+    postMessage('synchronized-data');
   }
 });
 
 self.addEventListener('fetch', (event: any) => {
   const supportedMethods = [
-    'POST',
-    'PUT',
-    'DELETE'
+    methods.post,
+    methods.put,
+    methods.delete,
   ];
 
   if (!supportedMethods.includes(event.request.method)) {
@@ -303,18 +430,20 @@ self.addEventListener('fetch', (event: any) => {
       return response;
     } catch (error) {
       const path = new URL(event.request.url).pathname;
-      if (path.startsWith('/api/') && !navigator.onLine) {
+      const offlineActive = await getOfflineSetting()
+      
+      if (path.startsWith('/api/') && !navigator.onLine && offlineActive) {
 
         let requestId = null;
 
-        if (event.request.method === 'POST') {
+        if (event.request.method === methods.post) {
           const {
             attributes: { offline_id }
           } = await transformReadableStreamToObject(event.request.clone().body);
           requestPOST.set(String(offline_id), event.request.clone());
         }
 
-        if (event.request.method === 'PUT') {
+        if (event.request.method === methods.put) {
           const id = parseUrlSegment(event.request.url, 'id');
 
           if (requestPOST.has(String(id))) {
@@ -322,20 +451,25 @@ self.addEventListener('fetch', (event: any) => {
           }
         }
 
-        if (event.request.method === 'DELETE') {
+        if (event.request.method === methods.delete) {
           const id = parseUrlSegment(event.request.url, 'id');
 
           if (requestPOST.has(String(id))) {
             requestId = id;
           }
         }
+
+        const generatedID = new Date().valueOf();
 
         await queue.pushRequest({
-          request: event.request,
+          request: event.request.clone(),
           metadata: {
-            requestId
+            requestId,
+            id: generatedID, // ID to identify the request in the queue
           }
         });
+
+        await saveExecutedRequests(event.request.clone(), generatedID, status.pending)
 
         postMessage('queue-request');
       }
