53aacf5b712a278a01dc3107865602d34cb9df4a
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-08-05 11:40:46 -0500

    Add filters to paginateCacheOffline function

--- src/plugins/paginateCacheOffline.js
+++ src/plugins/paginateCacheOffline.js
@@ -1,18 +1,17 @@
 import { cache } from 'src/plugins/utils';
 
-export default async function paginateCacheOffline(fountain, search = null, page = 1, perPage = 10) {
+export default async function paginateCacheOffline(fountain, search = null, page = 1, perPage = 10, filters=null) {
   let data = null;
 
   if (typeof fountain === 'string') {
     data = await cache.get.item(`${fountain}::offline`) || { data: [] };
   }
-    
 
   if (typeof fountain === 'object') data = fountain;
 
   if (!data.data.length === 0) return data;
 
-  const filteredData = data.data.filter(item => {
+  const processedData = data.data.filter(item => {
     return Object.values(item).some(value => {
       if (value) {
         search = search ? search.toLowerCase() : null;
@@ -25,6 +24,28 @@ export default async function paginateCacheOffline(fountain, search = null, page
     });
   });
 
+  const applyFilters = (data, filters) => {
+    if (!filters) return data
+    return data.filter(item => {
+      return Object.entries(filters).every(([key, valueFilter]) => {
+        if (!(key in item)) {
+          return true;
+        }
+        
+        if (Array.isArray(valueFilter)) {
+          return valueFilter.includes(item[key]);
+        }
+        
+        if (typeof valueFilter === 'function') {
+          return valueFilter(item[key]);
+        }
+        
+        return item[key] === valueFilter;
+      });
+    });
+  }
+
+  const filteredData = applyFilters(processedData, filters);
   const totalPages = Math.ceil(filteredData.length / perPage);
   const startIndex = (page - 1) * perPage;
   const endIndex = startIndex + perPage;
