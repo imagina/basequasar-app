d6b0bedfea0f661eb6a1aea3af0c6c5d82165f79
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-08-05 11:22:41 -0500

    Fix: Redirecting to the update-app page when it is not necessary.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { eventBus } from 'src/plugins/utils';
 
 export default defineComponent({
   name: 'App',
@@ -12,11 +11,6 @@ export default defineComponent({
   {
     this.$q.iconSet.arrow.dropdown = 'fa fa-caret-down';
     this.$q.iconSet.expansionItem.icon = 'fa fa-chevron-down';
-    // Listen Service worker updates
-    eventBus.on('service-worker.update.available', () =>
-    {
-      this.$router.push({ name: 'app.update.app' });
-    });
   }
 });
 </script>
