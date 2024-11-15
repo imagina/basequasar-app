aef733f528b8b9110bc40fe8cf62cf7a92a58bb6
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-11-15 12:56:22 -0500

    Fixed: Move siteActions to respective modules

--- src/components/siteActions/controller.ts
+++ src/components/siteActions/controller.ts
@@ -55,151 +55,119 @@ export default function controller(props: ControllerProps) {
       }
 
       const { customBtns, customMenus } = state.customActions;
+      const defOrder = 9
+      const badge = state.badge
+      const keyBadge = Object.keys(badge)
+
+      const orderedBtns = [
+        //Go To Site
+        {
+          vIf: computeds.showGoToSiteButton?.value,
+          order: 1,
+          name: 'goToSite',
+          label: i18n.tr('isite.cms.configList.goToSite'),
+          props: {
+            label: i18n.tr('isite.cms.showSite'),
+            type: 'a',
+            id: 'siteActionGoToSite',
+            href: goToSiteUrl,
+            target: '_blank',
+            round: false,
+            rounded: true,
+            padding: 'xs md'
+          }
+        },
+        //Help Center
+        {
+          name: 'helpCenter',
+          desc: i18n.tr('isite.cms.message.descriptionHelpCenter'),
+          label: i18n.trp('isite.cms.label.helpCenter'),
+          vIf: parseInt(store.getSetting('isite::hcStatus') || '0'),
+          props: {
+            id: 'siteActionHelpCenter',
+            icon: 'fal fa-question-circle'
+          },
+          menu: {
+            actions: [
+              {
+                icon: 'fa-light fa-question-circle',
+                rightIcon: 'fa-light fa-chevron-right',
+                label: 'FAQ',
+                action: () => eventBus.emit('toggleHelpSection', {sectionName: 'faq'})
+              }
+            ]
+          }
+        },
+        // Clear Cache
+        {
+          name: 'clearCache',
+          label: i18n.tr('isite.cms.configList.clearCache'),
+          props: {
+            icon: 'fa-light fa-broom'
+          },
+          vIf: !computeds.isAppOffline.value,
+          action: () => router.push({
+            name: 'app.update.app',
+            query: {fromCache: 1}
+          })
+        },
+        ...customBtns
+      ].map(btn => {
+        const button = btn;
+        const name = btn.badgeName || btn.name || ''
+        if(keyBadge.includes((name))) {
+          button.badge = badge[name]
+        }
+        return button
+      }).sort((a, b) => (a.order || defOrder) - (b.order || defOrder));
+
+      const orderedMenus = [
+        //Profile
+        {
+          name: 'profile',
+          props: {
+            label: i18n.tr('iprofile.cms.sidebar.panelProfile'),
+            icon: 'fa-light fa-circle-user',
+            round: false,
+            square: true,
+            align: 'left',
+            id: 'profileButton'
+          },
+          action: () => (router.route.name != 'user.profile.me') ? router.push({name: 'user.profile.me'}) : null
+        },
+        //Settings
+        {
+          name: 'settings',
+          vIf: (config('app.mode') == 'iadmin'),
+          props: {
+            label: i18n.trp('isite.cms.label.setting'),
+            icon: 'fa-light fa-folder-gear',
+            round: false,
+            square: true,
+            align: 'left'
+          },
+          action: () => eventBus.emit('toggleMasterDrawer', 'config')
+        },
+        //logout
+        {
+          name: 'settings',
+          vIf: (config('app.mode') == 'iadmin') && !computeds.isAppOffline.value,
+          props: {
+            label: i18n.tr('isite.cms.configList.signOut'),
+            icon: 'fa-light fa-right-from-bracket',
+            round: false,
+            square: true,
+            textColor: 'red',
+            align: 'left'
+          },
+          action: () => methods.logout()
+        },
+        ...customMenus
+      ].sort((a, b) => (a.order || defOrder) - (b.order || defOrder));
 
       return {
-        buttons: [
-          //Offline
-          {
-            vIf: store.getSetting('isite::offline'),
-            name: 'offline',
-            label: i18n.tr('isite.cms.label.offline'),
-            badgeColor: 'orange',
-            badgeLabel: store.state.qofflineMaster.pendingRequests,
-            props: {
-              icon: 'fa-light fa-cloud-slash',
-              class: 'btn-small'
-            },
-            action: () => eventBus.emit('toggleMasterDrawer', 'offline')
-          },
-          //Go To Site
-          {
-            vIf: computeds.showGoToSiteButton?.value,
-            name: 'goToSite',
-            label: i18n.tr('isite.cms.configList.goToSite'),
-            props: {
-              label: i18n.tr('isite.cms.showSite'),
-              type: 'a',
-              id: 'siteActionGoToSite',
-              href: goToSiteUrl,
-              target: '_blank',
-              round: false,
-              rounded: true,
-              padding: 'xs md'
-            }
-          },
-          //checking
-          {
-            name: 'checking',
-            vIf: (config('app.mode') == 'ipanel') && store.hasAccess('icheckin.shifts.create'),
-            label: i18n.tr('icheckin.cms.sidebar.checkin'),
-            props: {
-              icon: 'fas fa-stopwatch',
-              round: true
-            },
-            action: () => eventBus.emit('toggleMasterDrawer', 'checkin')
-          },
-          //Chat
-          {
-            name: 'chat',
-            label: 'Chat',
-            vIf: (config('app.mode') == 'iadmin') && store.hasAccess('ichat.conversations.index'),
-            badge: state.badge.chat,
-            props: {
-              icon: 'fa-light fa-message-lines',
-              class: 'btn-small'
-            },
-            action: () => eventBus.emit('toggleMasterDrawer', 'chat')
-          },
-          //Notifications
-          {
-            name: 'notifications',
-            label: i18n.trp('isite.cms.label.notification'),
-            badge: state.badge.notification,
-            props: {
-              icon: 'fa-light fa-bell',
-              class: 'btn-small'
-            },
-            action: () => eventBus.emit('toggleMasterDrawer', 'notification')
-          },
-          //Help Center
-          {
-            name: 'helpCenter',
-            desc: i18n.tr('isite.cms.message.descriptionHelpCenter'),
-            label: i18n.trp('isite.cms.label.helpCenter'),
-            vIf: parseInt(store.getSetting('isite::hcStatus') || '0'),
-            props: {
-              id: 'siteActionHelpCenter',
-              icon: 'fal fa-question-circle'
-            },
-            menu: {
-              actions: [
-                {
-                  icon: 'fa-light fa-question-circle',
-                  rightIcon: 'fa-light fa-chevron-right',
-                  label: 'FAQ',
-                  action: () => eventBus.emit('toggleHelpSection', {sectionName: 'faq'})
-                }
-              ]
-            }
-          },
-          // Clear Cache
-          {
-            name: 'clearCache',
-            label: i18n.tr('isite.cms.configList.clearCache'),
-            props: {
-              icon: 'fa-light fa-broom'
-            },
-            vIf: !computeds.isAppOffline.value,
-            action: () => router.push({
-              name: 'app.update.app',
-              query: {fromCache: 1}
-            })
-          },
-          ...customBtns
-        ],
-        menu: [
-          //Profile
-          {
-            name: 'profile',
-            props: {
-              label: i18n.tr('iprofile.cms.sidebar.panelProfile'),
-              icon: 'fa-light fa-circle-user',
-              round: false,
-              square: true,
-              align: 'left',
-              id: 'profileButton'
-            },
-            action: () => (router.route.name != 'user.profile.me') ? router.push({name: 'user.profile.me'}) : null
-          },
-          //Settings
-          {
-            name: 'settings',
-            vIf: (config('app.mode') == 'iadmin'),
-            props: {
-              label: i18n.trp('isite.cms.label.setting'),
-              icon: 'fa-light fa-folder-gear',
-              round: false,
-              square: true,
-              align: 'left'
-            },
-            action: () => eventBus.emit('toggleMasterDrawer', 'config')
-          },
-          //logout
-          {
-            name: 'settings',
-            vIf: (config('app.mode') == 'iadmin') && !computeds.isAppOffline.value,
-            props: {
-              label: i18n.tr('isite.cms.configList.signOut'),
-              icon: 'fa-light fa-right-from-bracket',
-              round: false,
-              square: true,
-              textColor: 'red',
-              align: 'left'
-            },
-            action: () => methods.logout()
-          },
-          ...customMenus
-        ],
+        buttons: orderedBtns,
+        menu: orderedMenus,
         ...(props.replaceActions || {})
       };
     }),
@@ -246,8 +214,8 @@ export default function controller(props: ControllerProps) {
           }
         };
         // Separate btn and menu
-        if (config.type === 'btn') customBtns.push(customConfig);
-        else if (config.type === 'menu') customMenus.push(customConfig);
+        if (config.menu) customMenus.push(customConfig);
+        else customBtns.push(customConfig);
       });
       state.customActions = {customBtns, customMenus}
     },
